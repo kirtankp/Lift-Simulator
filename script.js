3d5d38af0e35f2ec924a4727438fd365217f61a7
kirtan <kirtan1902@gmail.com>
2023-05-28 10:48:43 +0530

    back to last commit

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-
-const lifts = []; // Array to store lift states
-const floorHeight = 100; // Height of each floor in pixels
-const liftSpeed = 2000; // Speed of lift movement in milliseconds
-
-//this function is called when the form is submitted, when the submit button is pressed
+//this function is called when the form is submitted, when the submit button is pressed.
 const Simulation = () => {
 
     //here we are storing the input value from the form.  
@@ -34,7 +29,7 @@ const generateFloors = (numFloors) => {
         `
         <div class="liftButtons">
             Floor ${numFloors}
-            <button onsubmit="handleLiftRequest(${0},${numFloors})" class="lift-control down">DOWN</button>
+            <button class="lift-control down">DOWN</button>
         </div>
     `;
     floorsContainer.appendChild(lastFloor);
@@ -47,9 +42,9 @@ const generateFloors = (numFloors) => {
         floor.innerHTML =
             `
             <div class="liftButtons">
-                <button onsubmit="handleLiftRequest(${0},${i})" class="lift-control up">UP</button>
+                <button class="lift-control up">UP</button>
                 Floor ${i}
-                <button onsubmit="handleLiftRequest(${0},${i})" class="lift-control down">DOWN</button>
+                <button class="lift-control down">DOWN</button>
             </div>
         `;
         floorsContainer.appendChild(floor);
@@ -62,7 +57,7 @@ const generateFloors = (numFloors) => {
     groundFloor.innerHTML =
         `
         <div class="liftButtons">
-            <button onsubmit="handleLiftRequest(${0},${0})" class="lift-control up">UP</button>
+            <button class="lift-control up">UP</button>
             Floor 0
         </div>
     `;
@@ -96,100 +91,4 @@ const generateLifts = (numLifts) => {
 
     }
 
-}
-
-
-// Step 4.3: Handle Lift Requests
-const handleLiftRequest = (liftIndex, floorIndex) => {
-    const lift = lifts[liftIndex];
-    const requestedFloor = floorIndex + 1;
-
-    // Add the requested floor to the targetFloors array for the corresponding lift
-    lift.targetFloors.push(requestedFloor);
-
-    // If the lift is currently stopped, start moving it towards the requested floor
-    if (lift.direction === 'stopped') {
-        moveLift(liftIndex);
-    }
-}
-
-// Step 4.4: Move Lift
-const moveLift = (liftIndex) => {
-    const lift = lifts[liftIndex];
-    const targetFloor = lift.targetFloors[0];
-
-    // Determine the direction of the lift movement
-    if (lift.currentFloor < targetFloor) {
-        lift.direction = 'up';
-    } else if (lift.currentFloor > targetFloor) {
-        lift.direction = 'down';
-    }
-
-    // Calculate the distance and duration for the lift movement
-    const distance = Math.abs(lift.currentFloor - targetFloor) * floorHeight;
-    const duration = distance * liftSpeed;
-
-    // Animate the lift movement
-    animateLift(liftIndex, targetFloor, duration, () => {
-        // Update the current floor and remove the reached target floor from the array
-        lift.currentFloor = targetFloor;
-        lift.targetFloors.shift();
-
-        // If there are more target floors, continue moving the lift
-        if (lift.targetFloors.length > 0) {
-            moveLift(liftIndex);
-        } else {
-            lift.direction = 'stopped';
-        }
-
-        // Call the updateDisplay function to show the updated lift status
-        updateDisplay();
-    });
-}
-
-// Step 4.5: Animate Lift Movement
-const animateLift = (liftIndex, targetFloor, duration, callback) => {
-    const liftElement = document.querySelectorAll('.lifts')[liftIndex];
-    const distance = Math.abs(lifts.currentFloor - targetFloor) * floorHeight;
-    const direction = lifts.currentFloor < targetFloor ? 1 : -1;
-
-    // Calculate the distance to move per frame
-    const distancePerFrame = (distance / duration) * 10;
-    let currentDistance = 0;
-
-    // Interval-based animation
-    const animationInterval = setInterval(() => {
-        if (currentDistance >= distance) {
-            clearInterval(animationInterval);
-            callback();
-        } else {
-            currentDistance += distancePerFrame;
-            liftElement.style.transform = `translateY(${direction * currentDistance}px)`;
-        }
-    }, 10);
-}
-
-
-// Step 4.6: Update Lift Status Display
-const updateDisplay = () => {
-    const statusDisplay = document.getElementById('statusDisplay');
-    statusDisplay.innerHTML = '';
-
-    lifts.forEach((lift, index) => {
-        statusDisplay.innerHTML += `Lift ${index + 1}: Current Floor - ${lift.currentFloor
-            }, Direction - ${lift.direction}<br>`;
-    });
-}
-
-const liftSimulate = () => {
-    // Initialize the state of each lift
-    for (let i = 0; i < numLifts; i++) {
-        lifts.push({
-            currentFloor: 0, // Starting floor for all lifts
-            direction: 'stopped', // Initial direction of all lifts
-            targetFloors: [], // Array to store requested floors for each lift
-        });
-    }
-    // Call the updateDisplay function to show the initial lift status
-    updateDisplay();
 }
